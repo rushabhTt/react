89beb816edfaf5e778655304e627b7bcccfabb01
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-08-16 19:43:49 +0530

    use of props

--- new-start/src/App.js
+++ new-start/src/App.js
@@ -1,10 +1,51 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "New York",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Los Angeles",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Chicago",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Houston",
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        // ? The <ExpenseItem /> syntax is a self-closing tag, which is a shorthand for <ExpenseItem></ExpenseItem>. This is a common practice in React when a component doesn’t have any children.
+        // ! here we are giving the values from above to the structure for the ExpenseItem
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
 }
--- new-start/src/components/ExpenseItem.js
+++ new-start/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-function ExpenseItem() {
-    return (
-      <div>
-        <h2>Expense Item!</h2>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__location">{props.location}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    );
-  }
-  
-  export default ExpenseItem;
+    </div>
+  );
+}
+
+export default ExpenseItem;
