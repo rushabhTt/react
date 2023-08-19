a21b133dab859ed7026f7c93bc15e3abb9914370
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-08-20 00:38:15 +0530

    Add files via upload

--- new-start/src/components/Expenses/ExpenseItem.js
+++ new-start/src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
@@ -5,12 +7,23 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // ! use useState inside the function
+  // console.log({props});
+  const [amount, setAmount] = useState(props.amount);
+  console.log({ amount, setAmount });
+
   const clickHandler = () => {
     console.log("clicked 😎");
   };
 
   const deleteHandler = () => {
     document.querySelector(".expense-item").remove();
+  };
+
+  const expenseTo100Handler = () => {
+    console.log("😉");
+    console.log(amount);
+    setAmount(100);
   };
 
   return (
@@ -21,11 +34,12 @@ const ExpenseItem = (props) => {
       <ExpenseDetails
         title={props.title}
         location={props.location}
-        amount={props.amount}
+        amount={amount}
       />
       {/* you just points to the function don't put () nahi toh pahle hi run ho jaayega*/}
-      <button onClick={clickHandler}>Click 🤑</button>
+      <button onClick={clickHandler}>Click</button>
       <button onClick={deleteHandler}>Delete 🗑️</button>
+      <button onClick={expenseTo100Handler}>expense: 💯</button>
     </Card>
   );
 };
