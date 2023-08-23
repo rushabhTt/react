9caeb10a9e0ff5fe5c6171fe0dd7f1b371481b78
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-08-24 00:33:34 +0530

    filter component is added from the code files

--- new-start/src/components/Expenses/Expenses.js
+++ new-start/src/components/Expenses/Expenses.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const [expenses, setExpenses] = useState(props.items);
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const deleteExpenseHandler = (id) => {
     setExpenses((prevExpenses) =>
@@ -13,19 +15,29 @@ const Expenses = (props) => {
     );
   };
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const expenseItems = expenses.map((expense) => {
     console.log(expense.id);
     return (
-      <ExpenseItem
-        // * The key prop is a unique identifier for an element in a list, but it is not passed down to the child component.
-        key={expense.id}
-        id={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        location={expense.location}
-        onDelete={deleteExpenseHandler}
-      />
+      <>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          // * The key prop is a unique identifier for an element in a list, but it is not passed down to the child component.
+          key={expense.id}
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+          onDelete={deleteExpenseHandler}
+        />
+      </>
     );
   });
 
