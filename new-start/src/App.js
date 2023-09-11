3cb12195e47450c1c749f885806288cb4ab6eced
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-09-11 05:51:07 +0530

    deleteExpenseHandler ki position changed, app.js me change

--- new-start/src/App.js
+++ new-start/src/App.js
@@ -1,41 +1,63 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "New York",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: "Los Angeles",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: "Chicago",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "Houston",
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: "New York",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: "Los Angeles",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: "Chicago",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: "Houston",
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // ! if updating based on previous snapshot then
+    // ! not correct way: setExpenses([expense, ...expenses]);
+    const newExpense = {
+      ...expense,
+      location: expense.location || "NZ",
+    };
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
+    // console.log(expense);
+    // setExpenses((prevExpenses) => {
+    //   return [expense, ...prevExpenses];
+    // });
+    // console.log("bye");
+  };
+
+  const deleteExpenseHandler = (expenseId) => {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== expenseId);
+    });
   };
 
   // return React.createElement(
@@ -48,9 +70,9 @@ function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expenses} onDelete={deleteExpenseHandler} />
     </div>
   );
-}
+};
 
 export default App;
--- new-start/src/components/Expenses/Expenses.js
+++ new-start/src/components/Expenses/Expenses.js
@@ -6,42 +6,45 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [expenses, setExpenses] = useState(props.items);
+  // const [expenses, setExpenses] = useState(props.items);
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const deleteExpenseHandler = (id) => {
-    setExpenses((prevExpenses) =>
-      prevExpenses.filter((expense) => expense.id !== id)
-    );
-  };
+  // const deleteExpenseHandler = (id) => {
+  //   setExpenses((prevExpenses) =>
+  //     prevExpenses.filter((expense) => expense.id !== id)
+  //   );
+  // };
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const expenseItems = expenses.map((expense) => {
-    console.log(expense.id);
+  // ! map converts one to other basically we are converting array to react
+  const expenseItems = props.items.map((expense) => {
+    console.log(expense);
     return (
-      <>
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        <ExpenseItem
-          // * The key prop is a unique identifier for an element in a list, but it is not passed down to the child component.
-          key={expense.id}
-          id={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-          onDelete={deleteExpenseHandler}
-        />
-      </>
+      <ExpenseItem
+        // * The key prop is a unique identifier for an element in a list, but it is not passed down to the child component.
+        key={expense.id}
+        id={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+        onDelete={props.onDelete}
+      />
     );
   });
 
-  return <Card className="expenses">{expenseItems}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expenseItems}
+    </Card>
+  );
 };
 
 export default Expenses;
