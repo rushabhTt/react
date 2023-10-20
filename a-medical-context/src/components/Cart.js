4be235b14032a4c552dc363db57f86ce9e6eb946
rushabhTt <144966257+rushabhTt@users.noreply.github.com>
2023-10-20 14:13:44 +0000

    empty whole cart button

--- a-medical-context/src/components/Cart.js
+++ a-medical-context/src/components/Cart.js
@@ -8,13 +8,13 @@ const Cart = ({
   return (
     <div className="cart-container">
       <h2>Cart</h2>
+      <button onClick={handleEmptyCart}>Empty whole Cart🗑️</button>
       <ul>
         {cart.map((item, index) => (
           <li key={index}>
             Key: {item.key} - Description: {item.description} - Price:{" "}
             {item.price} - Quantity: {item.quantity}
             <button onClick={() => handleRemoveItem(item)}>-1</button>
-            <button onClick={handleEmptyCart}>🗑️</button>
             <button onClick={() => handleAddItem(item)}>+1</button>
           </li>
         ))}
