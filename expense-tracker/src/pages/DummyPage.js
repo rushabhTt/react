5d6216ba65293542c1650c1b97c473ed267865ce
rushabhTt <144966257+rushabhTt@users.noreply.github.com>
2023-11-25 10:04:13 +0000

    isProfileComplete

--- expense-tracker/src/pages/DummyPage.js
+++ expense-tracker/src/pages/DummyPage.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function DummyPage() {
+  const [isProfileComplete, setIsProfileComplete] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const idToken = localStorage.getItem("idToken");
+        const response = await axios.post(
+          `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyD3D8fP7LX24FGdE7S1ivZZcvu98Ikt2pQ`,
+          {
+            idToken: idToken,
+          }
+        );
+        console.log(response.data, isProfileComplete);
+        const userData = response.data.users[0];
+        const profileComplete = !! userData;
+        setIsProfileComplete(profileComplete);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchData();
+  }, []); 
+
   return (
     <div className="text-right">
-      your profile is incomplete:
-      <Link to="/profile" className="text-blue-500 underline">
-        Complete your profile!
-      </Link>
+      {isProfileComplete ? (
+        "Your profile is complete."
+      ) : (
+        <>
+          Your profile is incomplete:
+          <Link
+            to="/profile"
+            className={`text-blue-500 ${
+              isProfileComplete ? "line-through" : "underline"
+            }`}
+            onClick={(e) => !isProfileComplete && e.preventDefault()}
+          >
+            Complete your profile!
+          </Link>
+        </>
+      )}
     </div>
   );
 }
