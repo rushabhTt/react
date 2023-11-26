9e4e9d81ad7ad436ba3540d25fcc1f6e86bb065b
rushabhTt <144966257+rushabhTt@users.noreply.github.com>
2023-11-26 23:26:09 +0000

    edit delete database

--- expense-tracker/src/components/ExpenseForm.js
+++ expense-tracker/src/components/ExpenseForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function ExpenseForm() {
   const [amount, setAmount] = useState("");
@@ -7,22 +9,23 @@ function ExpenseForm() {
   const [category, setCategory] = useState("");
   const [expenses, setExpenses] = useState([]);
   const [submitting, setSubmitting] = useState(false);
+  const [editingExpense, setEditingExpense] = useState(null);
 
   useEffect(() => {
     const fetchExpenses = async () => {
       try {
-        const response = await axios.get(
+        const { data: expensesData } = await axios.get(
           "https://expensetracker-12365-default-rtdb.firebaseio.com/expense.json"
         );
-        // Assuming the response.data is an object with expense items
-        const expensesData = response.data;
-        // * Agar expensesData ek object hai, toh isse ek array mein convert karo
-        const expensesArray = Array.isArray(expensesData)
-          ? expensesData
-          : expensesData
-          ? Object.values(expensesData)
-          : [];
+        const expensesArray = Object.entries(expensesData || {}).map(
+          // * each element will be stored as id and ...data in the expensesArray
+          ([firebaseKey, data]) => ({
+            id: firebaseKey,
+            ...data,
+          })
+        );
         setExpenses(expensesArray);
+        console.log(expensesArray);
       } catch (error) {
         console.error("Error fetching expenses:", error);
       }
@@ -32,21 +35,31 @@ function ExpenseForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(amount, description, category);
     try {
       setSubmitting(true);
-      const response = await axios.post(
-        "https://expensetracker-12365-default-rtdb.firebaseio.com/expense.json",
-        {
-          amount: parseFloat(amount),
-          description: description,
-          category: category,
-        }
-      );
-      console.log("Expense submitted successfully:", response.data);
-
-      setExpenses((prevExpenses) => [...prevExpenses, response.data]);
-
+      const newData = {
+        amount: parseFloat(amount),
+        description: description,
+        category: category,
+      };
+      if (editingExpense) {
+        await axios.put(
+          `https://expensetracker-12365-default-rtdb.firebaseio.com/expense/${editingExpense.id}.json`,
+          newData
+        );
+        setExpenses((prevExpenses) =>
+          prevExpenses.map((item) =>
+            item.id === editingExpense.id ? { ...item, ...newData } : item
+          )
+        );
+        setEditingExpense(null);
+      } else {
+        await axios.post(
+          "https://expensetracker-12365-default-rtdb.firebaseio.com/expense.json",
+          newData
+        );
+        setExpenses((prevExpenses) => [...prevExpenses, newData]);
+      }
       setAmount("");
       setDescription("");
       setCategory("");
@@ -56,6 +69,34 @@ function ExpenseForm() {
       setSubmitting(false);
     }
   };
+
+  const handleDelete = async (expense) => {
+    console.log("Deleting expense of id:", expense.id);
+    try {
+      await axios.delete(
+        `https://expensetracker-12365-default-rtdb.firebaseio.com/expense/${expense.id}.json`
+      );
+      setExpenses((prevExpenses) =>
+        prevExpenses.filter((item) => item.id !== expense.id)
+      );
+      console.log("Expense successfully deleted");
+    } catch (error) {
+      console.error("Error deleting expense:", error);
+    }
+  };
+
+  const handleEdit = (expense) => {
+    try {
+      setEditingExpense(expense);
+
+      setAmount(expense.amount.toString());
+      setDescription(expense.description);
+      setCategory(expense.category);
+    } catch (error) {
+      console.error("Error editing expense:", error);
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white rounded-md shadow-md">
       <h2 className="text-2xl font-semibold mb-4">Expense Form</h2>
@@ -113,15 +154,55 @@ function ExpenseForm() {
       </form>
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-4">Expenses</h2>
-        <ul>
-          {expenses &&
-            expenses.map((expense) => (
-              <li key={expense.id}>
-                <strong>{expense.category}</strong>: ₹{expense.amount} -{" "}
-                {expense.description}
-              </li>
-            ))}
-        </ul>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Category
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Amount
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {expenses &&
+                expenses.map((expense) => (
+                  <tr
+                    key={expense.id}
+                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                  >
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {expense.category}
+                    </td>
+                    <td class="px-6 py-4">₹{expense.amount}</td>
+                    <td class="px-6 py-4">{expense.description}</td>
+                    <td class="px-6 py-4">
+                      <button
+                        className="ml-2 text-blue-500"
+                        onClick={() => handleEdit(expense)}
+                      >
+                        <FontAwesomeIcon icon={faEdit} />
+                      </button>
+                      <button
+                        className="ml-2 text-red-500"
+                        onClick={() => handleDelete(expense)}
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- expense-tracker/src/index.js
+++ expense-tracker/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faEdit, faTrash);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
