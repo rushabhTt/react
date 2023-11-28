08e1f2828d059af91c40c84fbc160e1846ad3ca6
rushabhTt <144966257+rushabhTt@users.noreply.github.com>
2023-11-28 12:12:53 +0000

    new form

--- redux-project/src/store/index.js
+++ redux-project/src/store/index.js
@@ -1,16 +1,29 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case "counter/incremented":
-      return { value: state.value + 1 };
-    case "counter/decremented":
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { value: 0 },
+  reducers: {
+    incremented: (state) => {
+      state.value += 1;
+    },
+    decremented: (state) => {
+      state.value -= 1;
+    },
+    incrementBy2: (state) => {
+      state.value += 2;
+    },
+    decrementBy2: (state) => {
+      state.value -= 2;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+export const { incremented, decremented, incrementBy2, decrementBy2 } =
+  counterSlice.actions;
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
